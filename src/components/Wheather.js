805057dc01c615e782c108e063fbe76fcd08f260
Onkar Patil <102043839+Foxhog-x@users.noreply.github.com>
2023-11-09 14:09:01 +0530

    added date&time, create  env to store api key

--- src/components/Wheather.js
+++ src/components/Wheather.js
@@ -4,15 +4,17 @@ const Wheather = () => {
   const [wheatherData, setWheatherData] = useState(null);
   const [cityName, setCityName] = useState("");
   const [temprature, setTemprature] = useState(null);
-  const [titleImage, setTitleImage] = useState(null);
+  const [titleImage, setTitleImage] = useState(null); //
   const [noCityFound, setNoCityFound] = useState(null);
   const [loader, setLoader] = useState(false);
-
-  const apiKey = "dd00f982e8d9c3f07a0f35f636352aa6";
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+  const apiKey = process.env.REACT_APP_API_KEY;
 
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
 
   const apiData = () => {
+    setNoCityFound(null);
     setWheatherData(null);
     setLoader(true);
     if (cityName !== "") {
@@ -21,6 +23,7 @@ const Wheather = () => {
         .then((data) => {
           // Handle the weather data here
           if (data.cod === "404") {
+            console.log("404 error");
             setWheatherData(null);
           } else {
             setWheatherData(data);
@@ -28,8 +31,25 @@ const Wheather = () => {
             setNoCityFound(null);
           }
 
-          const iconCode = data ? data?.weather[0]?.icon : null;
+          const offsetSeconds = data.timezone;
 
+          const now = new Date();
+
+          const targetTime = new Date(now.getTime() + offsetSeconds * 1000);
+
+          const year = targetTime.getUTCFullYear();
+          const month = targetTime.getUTCMonth() + 1;
+          const day = targetTime.getUTCDate();
+          const hours = targetTime.getUTCHours();
+          const minutes = targetTime.getUTCMinutes();
+
+          const formatedTime = `${hours}:${minutes}`;
+          const formateddate = `${day}.${month}.${year}`;
+
+          setTime(formatedTime);
+          setDate(formateddate);
+
+          const iconCode = data ? data?.weather[0]?.icon : null;
           const temp = Math.round(data.main.temp - 273.15);
           const mainImage = `http://openweathermap.org/img/w/${iconCode}.png`;
           setTemprature(temp);
@@ -46,64 +66,73 @@ const Wheather = () => {
     }
   };
 
-  console.log(cityName !== null || cityName !== "", "boolean");
-
   const handleChange = (event) => {
     setCityName(event.target.value);
   };
 
+  console.log(wheatherData);
   return (
-    <div className="main_container">
-      <div className="input_container">
+    <div className="main-container">
+      <div className="search-div">
         <input
           className="search-bar"
           type="text"
-          placeholder="Search here"
+          placeholder="Search City here"
           onChange={handleChange}
         />
         <button onClick={() => apiData()}>Search</button>
       </div>
       {loader && <p>Loading Data...</p>}
       {noCityFound !== null ? <h3>{noCityFound}</h3> : null}
-      <div>
-        {wheatherData !== null ? (
-          <div className="main">
-            <div className="container">
-              <h2>{wheatherData.name}</h2>
-              <div className="image">
-                <img src={titleImage} alt="wheather img" width="100px" />
-                <h1>{wheatherData.weather[0].main} </h1>
-              </div>
-              <h3>{wheatherData.weather[0].description}</h3>
-              <div className="temp_and_humidity">
-                <div className="temprature-section">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/728/728093.png"
-                    alt=""
-                    width="60px"
-                  />
-                  <h3>{wheatherData.main.humidity}</h3>
-                  <p>Humidity:</p>
-                </div>
-                <div className="humidity-section">
-                  <img
-                    src="https://cdn.pixabay.com/photo/2022/06/01/05/54/weather-7234860_1280.png"
-                    alt=""
-                    width="40px"
-                  />
-                  <h3>
-                    {temprature}
-                    <span>&#8451;</span>
-                  </h3>
-                  <p>Temprature:</p>
-                </div>
+      {wheatherData !== null ? (
+        <div className="container">
+          <div className="left-container">
+            <div className="last-flex">
+              <p>
+                {wheatherData.name} {wheatherData.sys.country}
+              </p>
+              <div className="date">
+                <p className="date">
+                  {date} {time}
+                </p>
               </div>
             </div>
+            <div className="left-flex-2">
+              <p className="main-temp">
+                {temprature}
+                <span>&#176;</span>
+              </p>
+              <div className="margin-top">
+                <p>{wheatherData.wind.speed} mph</p>
+                <p> 60 humidity</p>
+              </div>
+            </div>
+            <div className="left-flex-3">
+              <h1>{wheatherData.weather[0].description}</h1>
+            </div>
           </div>
-        ) : (
-          ""
-        )}
-      </div>
+
+          <div className="right-container">
+            <h1>Good Morning</h1>
+            <h2>12.27pm</h2>
+            <div className="right-internal">
+              <p className="p-temp">
+                {temprature}
+                <span>&#176;</span>
+              </p>
+              <div className="vertical"></div>
+              <div className="right">
+                <p> {wheatherData.wind.speed} mph</p>
+                <p> {wheatherData.main.humidity} humidity</p>
+              </div>
+            </div>
+
+            <span>Feels like</span>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
